11dcf980dbba7b2165f455c02811cb720c84dc91
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-01-06 13:17:23 -0700

    📚 Simple Python Script to Install Dependencies

---
+++ installDependencies.py
@@ -0,0 +1,33 @@
+from subprocess import check_output
+from os import system
+
+pips = ['pip', 'pip3']
+dependency_list = ['pandas', 'geocoder']
+
+
+def bash_command(user_in):
+    _ = system(user_in)
+
+
+def clear_screen():
+    bash_command('clear')
+
+
+clear_screen()
+pip_installed = False
+while not pip_installed:
+    for pip in pips:
+        try:
+            output = check_output(f'{pip} --version', shell=True)
+            pip_on_this_machine = pip
+            pip_installed = True
+            break
+        except Exception:
+            pip_installed = False
+installed_dependencies = ''
+for dependencies in dependency_list:
+    bash_command(f'{pip} install {dependencies}')
+    installed_dependencies += f'{dependencies}\n'
+
+clear_screen()
+print(f'All dependencies are installed:\n{installed_dependencies}')
